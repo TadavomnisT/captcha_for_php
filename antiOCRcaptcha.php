d6085cd7a3a9e0eaa9027de636fcea52f16cb66a
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2021-11-07 17:18:19 +0330

    Changing implementation of Random-Text-Generator
    Changing implementation of Random-Text-Generator .

--- antiOCRcaptcha.php
+++ antiOCRcaptcha.php
@@ -6,17 +6,18 @@
 
 session_start();
 $captcha_length = 32; //Define length of captcha characters, must be from 1 to 32
-$case_sensitivity = true; //Put true if you want to have case sensitive captcha and false if you don't
-$str_rand=md5(mt_rand());
-$str="";
+$case_sensitivity = false; //Put true if you want to have case sensitive captcha and false if you don't
+$str_rand = "";
+for ($i=0; $i < $captcha_length ; $i++) $str_rand .= array_merge(range('a', 'z'), range(0 , 9))[ rand(0,35) ];
+$str = "";
 
-$image_l=30.8*$captcha_length+((intval($captcha_length/14)==0)?pow($captcha_length,(1/3))*8:0);
+$image_l = 30.8*$captcha_length+((intval($captcha_length/14)==0)?pow($captcha_length,(1/3))*8:0);
 $newImage = imagecreate($image_l,80);
 imagecolorallocate($newImage,220,220,255);
-$col=imagecolorallocate($newImage,0,0,0);
+$col = imagecolorallocate($newImage,0,0,0);
 
-$fontn[0]=realpath(getcwd()."/zxx-noise.ttf");
-$fontn[1]=realpath(getcwd()."/zxx-xed.ttf");
+$fontn[0] = realpath(getcwd()."/zxx-noise.ttf");
+$fontn[1] = realpath(getcwd()."/zxx-xed.ttf");
 
 for ($i=0;$i<100;$i++)imageline($newImage,mt_rand(0,$image_l),mt_rand(0,80),mt_rand(0,$image_l),mt_rand(0,80),imagecolorallocate($newImage,245,245,245));
 for ($i=0; $i < 15 ; $i++) $cols[] = imagecolorallocate ($newImage, mt_rand(0,100), mt_rand(0,100), mt_rand(0,100));
@@ -27,7 +28,7 @@ for ($i=0; $i < $captcha_length ; $i++){
     if(mt_rand(0,1))imagettftext($newImage, mt_rand(30,50), mt_rand(-20,20), mt_rand(0,$image_l), mt_rand(0,80), $cols[array_rand($cols)], $fontn[mt_rand(0,1)], " " );
     $str.=$str_temp;
 }
-if ($case_sensitivity==false)$str=strtolower($str);
+if ($case_sensitivity == false)$str=strtolower($str);
 $_SESSION['untiOCRcaptcha']=$str;
 
 header('content:image/jpeg');
